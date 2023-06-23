a399fce3c046a96fbe6be05229688111a551ed8a
Colin Alves <alves.colin@icloud.com>
2023-06-23 10:37:21 +0200

    first step

---
+++ RuneMaker/RuneMaker/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RuneMaker;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ RuneMaker/RuneMaker/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RuneMaker;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
